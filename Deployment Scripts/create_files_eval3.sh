a4c5d08ddcdd7f3576787371f539cc0a0379ea16
EC2 Default User <ec2-user@ip-172-31-10-171.us-east-2.compute.internal>
2021-03-17 04:55:52 +0000

    Added all evals

--- Deployment Scripts/create_files_eval3.sh
+++ Deployment Scripts/create_files_eval3.sh
@@ -3,9 +3,9 @@
 output_folder=$1
 node_number=$2
 
-file_sizes=(128, 512, 2000, 8000, 32000)
+file_sizes=(128 512 2000 8000 32000)
 
 for file_size in ${file_sizes[@]};
 do
-	dd if=/dev/zero of="$output_folder"/file_$node_number_$file_size.txt  bs=$file_size  count=1	
+	dd if=/dev/zero of="$output_folder"/files/file_${node_number}_$file_size.txt  bs=$file_size  count=1	
 done
